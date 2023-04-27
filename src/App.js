b8d3544d7c62e90544f48fbb9284ddfcc2997e64
dhruba <dhruba@gmail.com>
2023-04-27 13:04:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isComplete, setIsComplete] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>my todos</h1>
+      <div className='todo-wrapper'>
+        <div className='input'>
+          <div className='input-item'>
+            <label>title</label>
+            <input type='text' placeholder='task title' />
+          </div>
+          <div className='input-item'>
+            <label>description</label>
+            <input type='text' placeholder='task description' />
+          </div>
+          <div className='input-item'>
+            <button type='button' className='btn'>Add</button>
+          </div>
+        </div>
+
+
+        <div className='btn-area'>
+          <button className={`secondaryBtn ${isComplete === false && 'active'}`}
+            onclick={() => setIsComplete(false)}>todo
+          </button>
+          <button className={`secondaryBtn ${isComplete === true && 'active'}`}
+            onClick ={() => setIsComplete(true)} >completed
+          </button>
+        </div>
+
+        <div className='todo-list'>
+
+          <div className='todo-list-item'>
+            <h3>task 1</h3>
+            <p>desription</p>
+          </div>
+
+          <div>
+            <AiOutlineDelete className='icon'/>
+          </div>
+
+        </div>
+
+      </div >
+    </div >
   );
 }
 
